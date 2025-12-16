de2c72345d33d13b1f1b9e4ad82a10aaf724f3e1
Tingting Wan <tingting.wan@databricks.com>
2025-12-16 14:44:12 +0000

    fix(template): replace relative import in utils.py to support streamlit run

--- streamlit-app-template/utils.py
+++ streamlit-app-template/utils.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from typing import Any, Dict, Optional
 import time
 
-from .config import get_data_source
+from config import get_data_source
 
 _provider = get_data_source()
 
